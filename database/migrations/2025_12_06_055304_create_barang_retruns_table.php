f211666da23c27748f2658b4786e1eb57079aecd
bayubudiana <bayubudiana93@gmail.com>
2025-12-06 15:00:49 +0800

    create migration

---
+++ database/migrations/2025_12_06_055304_create_barang_retruns_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang_retruns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('barang_id')->constrained('data_barangs')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->integer('jumlah_retrun');
+            $table->string('deskripsi')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_retruns');
+    }
+};
